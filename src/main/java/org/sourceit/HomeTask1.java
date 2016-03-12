71f5f6242c92cdf91f496a903647ed59ede779e3
kuzmiwee <kzwork22@gmail.com>
2016-03-12 08:04:11 +0200

    First try

--- src/main/java/org/sourceit/HomeTask1.java
+++ src/main/java/org/sourceit/HomeTask1.java
@@ -1,7 +1,8 @@
 package org.sourceit;
 
-public class HomeTask1 {
+import java.util.Random;
 
+public class HomeTask1 {
     /**
      * Проверить, является ли число четным.
      *
@@ -22,7 +23,13 @@ public class HomeTask1 {
      * @return значение гипотенузы.
      */
     public static double findHypotenuse(double a, double b) {
-        return 0D;
+        double hypotenuseTriangle;
+        if ((a <= 0) || (b <= 0)) {
+            return (0);
+        } else {
+            hypotenuseTriangle = Math.sqrt((a * a) + (b * b));
+            return (hypotenuseTriangle);
+        }
     }
 
     /**
@@ -34,11 +41,13 @@ public class HomeTask1 {
      * @return периметр треугольника.
      */
     public static double perimeter(double a, double b, double c) {
-        return 0D;
+        double perimeterTriangle;
+        perimeterTriangle = (a + b + c);
+        return (perimeterTriangle);
     }
 
     /**
-     * Метод находит площадь треугольника.
+     * Метод находит площадь прямоугольного треугольника.
      * Значения должны быть больше 0.
      *
      * @param a
@@ -46,7 +55,11 @@ public class HomeTask1 {
      * @return площадь треугольника.
      */
     public static double area(double a, double b) {
-        return 0D;
+        double areaTriangle;
+        if ((a <= 0) || (b <= 0)) {
+            areaTriangle = ((a * b) / 2);
+            return (areaTriangle);
+        } else return (0);
     }
 
     /**
@@ -57,7 +70,10 @@ public class HomeTask1 {
      * @return сгенерированное число.
      */
     public static int generateNumberFromRange(int min, int max) {
-        return 0;
+        int randomInterger;
+        Random r = new Random();
+        randomInterger = (r.nextInt(max) + min);
+        return (randomInterger);
     }
 
     /**
@@ -69,7 +85,13 @@ public class HomeTask1 {
      * @return сумма цифр.
      */
     public static long calculateSum(long number) {
-        return 0L;
+        long numberLocal = number;
+        long sum = 0;
+        while (numberLocal > 0) {
+            sum += number % 10;
+            numberLocal = numberLocal / 10;
+        }
+        return (sum);
     }
 
     /**
@@ -81,7 +103,15 @@ public class HomeTask1 {
      * @return элемент последовательности.
      */
     public static int fibonacci(int till) {
-        return 0;
+        int[] fibonacciArray = new int[till];
+        int i = 2;
+        fibonacciArray[0] = 1;
+        fibonacciArray[1] = 1;
+        while (i < till) {
+            fibonacciArray[i] = fibonacciArray[i - 1] + fibonacciArray[i - 2];
+            i++;
+        }
+        return (fibonacciArray[till - 1]);
     }
 
     /**
@@ -93,6 +123,20 @@ public class HomeTask1 {
      * @return является ли билет счастливым.
      */
     public static boolean isHappy(long ticket) {
+        long ticketLocal = ticket, sumLeft = 0, sumRight = 0;
+        int i = 0;
+        if ((ticketLocal >=1 ) && (ticketLocal <= 999999)) {
+            while (ticketLocal > 0) {
+                while (i<3) {
+                    sumRight += ticketLocal % 10;
+                    ticketLocal = ticketLocal / 10;
+                    i++;
+                }
+                sumLeft += ticketLocal % 10;
+                ticketLocal = ticketLocal / 10;
+            }
+            return (sumLeft == sumRight);
+        }
         return false;
     }
 }
